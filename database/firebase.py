d7fefd33f2cc7eccac19e40b1145edb378de2e27
test-razor-pay <wanteddeveloper0@gmail.com>
2026-01-05 12:27:36 +0530

    Add files via upload

--- database/firebase.py
+++ database/firebase.py
@@ -1,1 +1,9 @@
+import firebase_admin, json, os
+from firebase_admin import credentials, firestore
 
+firebase_json = os.getenv("FIREBASE_JSON")
+
+cred = credentials.Certificate(json.loads(firebase_json))
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
