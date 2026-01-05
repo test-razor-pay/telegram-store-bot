f4522a1fc860ecefc7c7b1845afb014a919e26a2
test-razor-pay <wanteddeveloper0@gmail.com>
2026-01-05 11:59:19 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config import BOT_TOKEN
+from handlers import user, admin
+
+bot = Bot(BOT_TOKEN)
+dp = Dispatcher()
+
+dp.include_router(user.router)
+dp.include_router(admin.router)
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
