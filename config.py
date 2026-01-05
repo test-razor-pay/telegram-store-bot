4b239035050a0d83aad2544251815d4394c34732
test-razor-pay <wanteddeveloper0@gmail.com>
2026-01-05 12:26:10 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,11 @@
+BOT_TOKEN = "8222319323:AAEMYWTedLW1H-6pk_7zpeFNEFPWxbk-jJU"
+
+ADMINS = [8053084391, 6695446010]
+
+UPI_ID = "paytm.s1oppzf@pty"
+UPI_NAME = "BANTI BHAIYA GAMING HUB"
+
+MERCHANT_ID = "vupXaw32751713519005"
+PAYMENT_API = "https://api.extracthub.fun/paytm-payment-gateway/index.php"
+
+QR_EXPIRY_SECONDS = 180
